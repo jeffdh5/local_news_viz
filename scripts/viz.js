4fa0972e89b6dd85eb8a10a50708a21ca5d36d57
Jeff <jeffdh5@gmail.com>
2014-04-05 01:18:26 -0700

    removed jquery, changed onclick class toggler to use virtual dom
    -removed query ajax call, replaced with XMLHttpRequest
    -changed previously_clicked to use state as opposed to creating a
    separate javascript object
    -class toggling done through render function in Article now

--- scripts/viz.js
+++ scripts/viz.js
@@ -3,77 +3,80 @@
 var previously_clicked = {}
 
 var Article = React.createClass({
-  render: function() {
-    var rawMarkup = (this.props.children).substring(0, 300) + "...";
-    return (
-      <div className="article" class="article">
-        <h2 className="articleTitle">{this.props.title.substring(0,100)}</h2>
-        <h4 className="articleAuthor">{this.props.author}</h4>
-        <p dangerouslySetInnerHTML={{__html: rawMarkup}} />
-      </div>
-    );
-  }
+	getInitialState: function() {
+		return {prev_clicked:false};
+	},
+
+	onClick: function() {
+		if (this.state.prev_clicked == false) {
+			this.setState({prev_clicked:true})
+		} else {
+			this.setState({prev_clicked:false})
+		}
+	},
+	
+	render: function() {
+		var rawMarkup = (this.props.children).substring(0, 300) + "...";
+		if (this.state.prev_clicked == false) {
+			var className="article"
+		} else {
+			var className="article-clicked"
+		}		
+		return (
+			<div className={className} onClick={this.onClick}>
+				<h2 className="articleTitle">{this.props.title.substring(0,100)}</h2>
+				<h4 className="articleAuthor">{this.props.author}</h4>
+				<p dangerouslySetInnerHTML={{__html: rawMarkup}} />
+			</div>
+		);
+	}
 });
 
 var ArticleBox = React.createClass({
-  loadArticlesFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this)
-    });
-  },
-  onClick: function(event) {
-  	
-  	target = event.target
-  	if (target.tagName != "DIV") {
-  		target = target.parentNode  		
+	loadArticlesFromServer: function() {
+		target = this
+		var http_request = new XMLHttpRequest();
+		http_request.open("GET", this.props.url, true);
+		http_request.onreadystatechange = function() {
+			var done=4;
+			var ok = 200;
+			if (http_request.readyState === done && http_request.status === ok){
+				target.setState({data: JSON.parse(http_request.responseText)});
+			}
+		};
+		http_request.send();
+	},
+	
+	getInitialState: function() {
+		return {data: []};
+	},
+	
+	componentWillMount: function() {
+		this.loadArticlesFromServer();
+		setInterval(this.loadArticlesFromServer, this.props.pollInterval);
+	},
+	
+	render: function() {
+		return (
+			<div className="articleBox">
+				<h1>Articles</h1>
+				<ArticleList data={this.state.data} />
+      	</div>
+    	);
   	}
-  
-  	if (previously_clicked[target.getAttribute("data-reactid")]) {
-  		console.log("Has been clicked before")
-	  	target.style.backgroundColor = "white"  
-	  	target.style.borderColor = "#ccc"
-	  	delete previously_clicked[target.getAttribute("data-reactid")]
-  	}
-  	else {
-		var lst_to_use = [target.getElementsByClassName("articleAuthor")[0].innerText, target.getElementsByClassName("articleTitle")[0].innerText]
-		previously_clicked[target.getAttribute("data-reactid")] = lst_to_use	
-		target.style.backgroundColor="#eee"  
-	  	target.style.borderColor = "black"
-  	}
-  },
-
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentWillMount: function() {
-    this.loadArticlesFromServer();
-    setInterval(this.loadArticlesFromServer, this.props.pollInterval);
-  },
-  render: function() {
-    return (
-      <div className="articleBox" onClick={this.onClick}>
-        <h1>Articles</h1>
-        <ArticleList data={this.state.data} />
-      </div>
-    );
-  }
 });
 
 var ArticleList = React.createClass({
-  render: function() {
-    var articleNodes = this.props.data.map(function (article, index) {
-    
-      return <Article key={index} author={article.author} title={article.title}>{article.text}</Article>;
-    });
-    return <div className="articleList">{articleNodes}</div>;
-  }
+	render: function() {
+		var articleNodes = this.props.data.map(function (article, index) {
+			return <Article key={index} author={article.author} title={article.title}>{article.text}</Article>;
+		});
+		return <div className="articleList">{articleNodes}</div>;
+  	}
 });
 
 
 React.renderComponent(
-  <ArticleBox url="/articles.json" pollInterval={2000} />,
-  document.getElementById('container')
+	<ArticleBox url="/articles.json" pollInterval={2000} />,
+	document.getElementById('container')
 );
